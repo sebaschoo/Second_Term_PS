9041f1a5720827a385842009823d8830a08455b9
Sebastian Melo <jsmelo48@student.ubc.ca>
2022-01-20 10:51:16 -0800

    Final

--- PS_0.jl
+++ PS_0.jl
@@ -303,57 +303,82 @@ plot(t, y_tau[3])
 plot(t, y_tau[4])
 
 # Rouwenhorst
-z = rouwenhorst(10,0.2,0.4,0)
-prob_rou = z[1]
-ybar_rou = @show Vector(z[2])
 
-y_rou = zeros(1000,1)
-pos = zeros(1000,1)
-y_rou[1] = ybar_tau[rand(1:10)]
+vec_matrix = [zeros(10,10), zeros(10), zeros(10,10), zeros(10), zeros(10,10), zeros(10), zeros(10,10), zeros(10)]
 
-for i=2:1000
-    x=rand()
-    for j=1:10
-        if y_rou[i-1]==ybar_rou[j]
-            pos[i] = j
-        end
-    end
-    prob_row = prob_rou[floor(Int, pos[i]),:]
-    thr_1 = prob_row[1]
-    thr_2 = thr_1 + prob_row[2]
-    thr_3 = thr_2 + prob_row[3]
-    thr_4 = thr_3 + prob_row[4]
-    thr_5 = thr_4 + prob_row[5]
-    thr_6 = thr_5 + prob_row[6]
-    thr_7 = thr_6 + prob_row[7]
-    thr_8 = thr_7 + prob_row[8]
-    thr_9 = thr_8 + prob_row[9]
-    if x<=thr_1
-        y_rou[i]=ybar_rou[1]
-    elseif x>thr_1 && x<=thr_2
-        y_rou[i] = ybar_rou[2]
-    elseif x>thr_2 && x<=thr_3
-        y_rou[i] = ybar_rou[3]
-    elseif x>thr_3 && x<=thr_4
-        y_rou[i] = ybar_rou[4]
-    elseif x>thr_4 && x<=thr_5
-        y_rou[i] = ybar_rou[5]
-    elseif x>thr_5 && x<=thr_6
-        y_rou[i] = ybar_rou[6]
-    elseif x>thr_6 && x<=thr_7
-        y_rou[i] = ybar_rou[7]
-    elseif x>thr_7 && x<=thr_8
-        y_rou[i] = ybar_rou[8]
-    elseif x>thr_8 && x<=thr_9
-        y_rou[i] = ybar_rou[9]
+for i in [0.2, 0.7, 0.9, 0.98]
+    z = rouwenhorst(10,i,0.4,0)
+    if i==0.2
+        vec_matrix[1] = z[1]
+        vec_matrix[2] = Vector(z[2])
+    elseif i==0.7
+        vec_matrix[3] = z[1]
+        vec_matrix[4] = Vector(z[2])
+    elseif i==0.9
+        vec_matrix[5] = z[1]
+        vec_matrix[6] = Vector(z[2])
     else
-        y_rou[i] = ybar_rou[10]
+        vec_matrix[7] = z[1]
+        vec_matrix[8] = Vector(z[2])
+    end
+end  
+
+y_rou = [zeros(1000,1), zeros(1000,1), zeros(1000,1), zeros(1000,1)]
+pos = [zeros(1000,1), zeros(1000,1), zeros(1000,1), zeros(1000,1)]
+
+
+for k=1:4
+
+    l = 2*k - 1
+    m = 2*k
+    y_rou[k][1] = vec_matrix[m][rand(1:3)]
+
+    for i=2:1000
+        x=rand()
+        for j=1:10
+            if y_rou[k][i-1]==vec_matrix[m][j]
+                pos[k][i] = j
+            end
+        end
+        prob_row = vec_matrix[l][floor(Int, pos[k][i]),:]
+        thr_1 = prob_row[1]
+        thr_2 = thr_1 + prob_row[2]
+        thr_3 = thr_2 + prob_row[3]
+        thr_4 = thr_3 + prob_row[4]
+        thr_5 = thr_4 + prob_row[5]
+        thr_6 = thr_5 + prob_row[6]
+        thr_7 = thr_6 + prob_row[7]
+        thr_8 = thr_7 + prob_row[8]
+        thr_9 = thr_8 + prob_row[9]
+        if x<=thr_1
+            y_rou[k][i]=vec_matrix[m][1]
+        elseif x>thr_1 && x<=thr_2
+            y_rou[k][i] = vec_matrix[m][2]
+        elseif x>thr_2 && x<=thr_3
+            y_rou[k][i] = vec_matrix[m][3]
+        elseif x>thr_3 && x<=thr_4
+            y_rou[k][i] = vec_matrix[m][4]
+        elseif x>thr_4 && x<=thr_5
+            y_rou[k][i] = vec_matrix[m][5]
+        elseif x>thr_5 && x<=thr_6
+            y_rou[k][i] = vec_matrix[m][6]
+        elseif x>thr_6 && x<=thr_7
+            y_rou[k][i] = vec_matrix[m][7]
+        elseif x>thr_7 && x<=thr_8
+            y_rou[k][i] = vec_matrix[m][8]
+        elseif x>thr_8 && x<=thr_9
+            y_rou[k][i] = vec_matrix[m][9]
+        else
+            y_rou[k][i] = vec_matrix[m][10]
+        end
     end
 end
 
 t = collect(range(1,stop=1000,length=1000))
-plot(t, y_rou)
-
+plot(t, y_rou[1])
+plot(t, y_rou[2])
+plot(t, y_rou[3])
+plot(t, y_rou[4])
 
 ##############
 # Question 5 #
