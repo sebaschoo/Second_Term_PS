f2899ee6836b621e2c9b9f8f90621bd75943dd74
Sebastian Melo <jsmelo48@student.ubc.ca>
2022-01-20 08:03:34 -0800

    Final

--- PS_0.jl
+++ PS_0.jl
@@ -128,36 +128,60 @@ function _rouwenhorst(p::Real, q::Real, m::Real, Δ::Real, n::Integer)
     end
 end
 
-z = rouwenhorst(3,0.2,0.4,0)
-prob_rou = z[1]
-ybar_rou = @show Vector(z[2])
+vec_matrix = [zeros(3,3), 1:1:length(1), zeros(3,3), 1:1:length(1), zeros(3,3), 1:1:length(1), zeros(3,3), 1:1:length(1)]
+
+for i in [0.2, 0.7, 0.9, 0.98]
+    z = rouwenhorst(3,i,0.4,0)
+    if i==0.2
+        vec_matrix[1] = z[1]
+        vec_matrix[2] = z[2]
+    elseif i==0.7
+        vec_matrix[3] = z[1]
+        vec_matrix[4] = z[2]
+    elseif i==0.9
+        vec_matrix[5] = z[1]
+        vec_matrix[6] = z[2]
+    else
+        vec_matrix[7] = z[1]
+        vec_matrix[8] = z[2]
+    end
+end
+
+y_rou = [zeros(1000,1), zeros(1000,1), zeros(1000,1), zeros(1000,1)]
+pos = [zeros(1000,1), zeros(1000,1), zeros(1000,1), zeros(1000,1)]
 
 #Simulating the time series
-y_rou = zeros(1000,1)
-pos = zeros(1000,1)
-y_rou[1] = ybar_rou[rand(1:3)]
+for k=1:4
 
-for i=2:1000
-    x=rand()
-    for j=1:3
-        if y_rou[i-1]==ybar_rou[j]
-            pos[i] = j
+    l = 2*k - 1
+    m = 2*k
+    y_rou[k][1] = vec_matrix[m][rand(1:3)]
+
+    for i=2:1000
+        x=rand()
+        for j=1:3
+            if y_rou[k][i-1]==vec_matrix[m][j]
+                pos[k][i] = j
+            end
         end
-    end
-    prob_row = prob_rou[floor(Int, pos[i]),:]
-    thr_1 = prob_row[1]
-    thr_2 = prob_row[1] + prob_row[2]
-    if x<=thr_1
-        y_rou[i]=ybar_rou[1]
-    elseif x>thr_1 && x<=thr_2
-        y_rou[i] = ybar_rou[2]
-    else
-        y_rou[i] = ybar_rou[3]
+        prob_row = vec_matrix[l][floor(Int, pos[k][i]),:]
+        thr_1 = prob_row[1]
+        thr_2 = thr_1 + prob_row[2]
+        if x<=thr_1
+            y_rou[k][i]=vec_matrix[m][1]
+        elseif x>thr_1 && x<=thr_2
+            y_rou[k][i] = vec_matrix[m][2]
+        else
+            y_rou[k][i] = vec_matrix[m][3]
+        end
     end
 end
 
 t = collect(range(1,stop=1000,length=1000))
-plot(t, y_rou)
+plot(t, y_rou[1])
+plot(t, y_rou[2])
+plot(t, y_rou[3])
+plot(t, y_rou[4])
 
 ##############
 # Question 3 #
